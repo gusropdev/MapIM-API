f20426c8521996b566494dbf7c80cdf1cbf88f36
gusropdev <gusropdev@hotmail.com>
2024-09-25 12:42:59 -0300

    Adicionado requisição com detalhes para obtenção do Room.Slug e Room.Id

--- MapIM/Controllers/SearchController.cs
+++ MapIM/Controllers/SearchController.cs
@@ -40,18 +40,87 @@ public class SearchController : ControllerBase
             .Where(x => x.Name.Contains(query))
             .Select(x => new SearchResultViewModel { Id = x.Id, Name = x.Name, Type = "Professor" });
 
-        var floorsTask = _context.Floors
-            .Where(x => x.Name.Contains(query))
-            .Select(x => new SearchResultViewModel { Id = x.Id, Name = x.Name, Type = "Andar" });
-
         var unifiedResults = await departmentsTask
                .Union(roomsTask)
                .Union(categoriesTask)
                .Union(professorsTask)
-               .Union(floorsTask)
                .ToListAsync();
 
         return Ok(unifiedResults);
     }
 
+    [HttpGet("details")]
+    public async Task<IActionResult> GetRoomDetailsAsync([FromQuery] int entityId, [FromQuery] string entityType)
+    {
+        object result = null;
+
+        switch (entityType.ToLower())
+        {
+            case "professor":
+                var professor = await _context.Professors
+                    .Include(x => x.Room)
+                    .FirstOrDefaultAsync(x => x.Id == entityId);
+
+                if (professor == null || professor.Room == null)
+                    return NotFound("Professor ou Sala não encontrados.");
+
+                result = new
+                {
+                    RoomId = professor.Room.Id,
+                    RoomSlug = professor.Room.Slug
+                };
+                break;
+
+
+            case "sala":
+                var room = await _context.Rooms
+                    .FirstOrDefaultAsync(x => x.Id == entityId);
+
+                if (room == null)
+                    return NotFound("Sala não encontrada");
+
+                result = new
+                {
+                    RoomId = room.Id,
+                    RoomSlug = room.Slug
+                };
+                break;
+
+
+            case "categoria":
+                var category = await _context.Categories
+                    .Include(x => x.Rooms)
+                    .FirstOrDefaultAsync(x => x.Id == entityId);
+
+                if (category == null || !category.Rooms.Any())
+                    return NotFound("Categoria ou salas não encontradas.");
+
+                result = new
+                {
+                    Rooms = category.Rooms.Select(x => new { x.Id, x.Slug })
+                };
+                break;
+
+
+            case "departamento":
+                var department = await _context.Departments
+                    .Include(x => x.Rooms)
+                    .FirstOrDefaultAsync(x => x.Id == entityId);
+
+                if (department == null || !department.Rooms.Any())
+                    return NotFound("Departamento ou salas não encontradas.");
+
+                result = new
+                {
+                    Rooms = department.Rooms.Select(x => new { x.Id, x.Slug })
+                };
+                break;
+
+
+            default:
+                return BadRequest();
+        }
+
+        return Ok(result);
+    }
 }
--- MapIM/obj/Release/net8.0/MapIM.AssemblyInfo.cs
+++ MapIM/obj/Release/net8.0/MapIM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MapIM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aa50bd74c2ce0e19bf7d4d30fc2c9619688d129")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+928f498c6d6cb23ad780698cc0c8d405332b34bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MapIM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MapIM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
